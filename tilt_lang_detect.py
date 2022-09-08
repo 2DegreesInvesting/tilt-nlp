8bbd3466e6a6bea9b967e9ef33fd679b90a78ccc
MirijaHa <mirja@2degrees-investing.org>
2022-09-08 11:37:17 +0200

    add google style doctring

--- tilt_lang_detect.py
+++ tilt_lang_detect.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 DetectorFactory.seed = 0 # setting seed to ensure reproducability
 def conf_ld_detect_language(text):
-    """Langugae detection wrapper.
+    """Language detection wrapper.
     
     Returns detected language (ISO-code) and confidence of detection. In case of 
-    failure of detection string "ident_fail" and a pd.NA value for confidence is 
+    failure of detection string 'ident_fail' and a pd.NA value for confidence is 
     returned.
     
     Args:
--- tilt_translate.py
+++ tilt_translate.py
@@ -1,9 +1,22 @@
 from textblob import TextBlob
 
-def translate_english(text, from_lang):
+def translate_english(text, from_lang, to = 'en'):
+    """Language translation wrapper.
+    
+    Returns a string holding provided text translated from original to target
+    language. In case of failure of translation string 'translation_fail' is 
+    returned.
+    
+    Args:
+        text (str): The string that shall be translated.
+        from_lang (str): ISO-code of original language of text.
+        to (str): ISO-code of target language of translation. Defaults to 'en'.
+    Returns:
+        str: Holding translated text.
+    """
     try:
         blob = TextBlob(text)
-        return(str(blob.translate(from_lang = from_lang, to = 'en')))
+        return(str(blob.translate(from_lang = from_lang, to = to)))
     except:
         return "translation_fail"
     return True

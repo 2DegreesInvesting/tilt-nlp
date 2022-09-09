c4f89dbc8e98273b9e5d8b1a1c7f1c2d5de52493
MirijaHa <88371355+MirijaHa@users.noreply.github.com>
2022-09-09 14:52:20 +0200

    Adapt readme (#1)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tilt-nlp
+
 Repo to store python-modules for NLP tasks in the tilt project
+
+-   **tilt_lang_detect:** reproducably returns detected language of a provided
+text and confidence of detection using langdetect
+-   **tilt_translate:** translates provided text from a provided original
+language to another using textblob
---
+++ tilt_lang_detect.py
@@ -0,0 +1,23 @@
+from langdetect import detect_langs, DetectorFactory
+import pandas as pd
+
+DetectorFactory.seed = 0 # setting seed to ensure reproducability
+def conf_ld_detect_language(text):
+    """Language detection wrapper.
+    
+    Returns detected language (ISO-code) and confidence of detection. In case of 
+    failure of detection string 'ident_fail' and a pd.NA value for confidence is 
+    returned.
+    
+    Args:
+        text (str): The string for which language shall be detected.
+    Returns:
+        tuple: A tuple holding ISO-code of detected language along with 
+        detection confidence.
+    """
+    try:
+        highest_conf = detect_langs(text)[0]
+        return highest_conf.lang, highest_conf.prob
+    except:   
+        return "ident_fail", pd.NA
+    return True
